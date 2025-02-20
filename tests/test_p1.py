15b2477bcdf4f40408c4d94ad67d1f2cd9404abc
Melric Lazaro <melriclazaro@gmail.com>
2025-02-20 12:02:45 +0000

    Updated tests

--- tests/test_p1.py
+++ tests/test_p1.py
@@ -2,3 +2,17 @@ import json
 
 import pytest
 from app import app
+
+@pytest.fixture
+def client():
+    # Configure Flask app for testing
+    app.config['TESTING'] = True
+    app.config['DEBUG'] = False
+
+    # Create a test client
+    with app.test_client() as client:
+        yield client
+
+def test_get_health(client):
+    response = client.get("/")
+    assert response.status_code == 200
